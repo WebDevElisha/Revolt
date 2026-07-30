d6a762b14330057abf819093267fcde80892a685
Gojo <ElishaCreatesWebs1@gmail.com>
2026-07-30 10:56:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,63 @@ const sidebar = document.getElementById('sidebar');
 sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 95,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.8,
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "size": {
+      "value": 3.5,
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.2,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    }
+  },
+  "retina_detect": true
+});
